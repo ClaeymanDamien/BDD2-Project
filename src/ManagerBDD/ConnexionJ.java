8d909a0fdd184cb8b5f0f245e830f1a0f1395446
DClaeyman <claeymand@gmail.com>
2019-05-06 22:15:37 +0200

    First Querry

--- src/ManagerBDD/ConnexionJ.java
+++ src/ManagerBDD/ConnexionJ.java
@@ -7,18 +7,40 @@ public class ConnexionJ {
 
 
 	public static void main(String[] args) {
-		Connection connection = connecterDB();
+		Connection db;
+		Statement st;
+		ResultSet rst;
+		
+		
+		try {
+			db = connexionDB();
+			st = db.createStatement();
+			rst = st.executeQuery("SELECT * FROM promotion");
+			
+			while (rst.next()) {
+				System.out.println(rst.getString("idPrmotion"));
+			}
+		}catch (Exception e) {
+			e.getStackTrace();
+		}
+		
 	}
-	public static Connection  connecterDB(){
+	public static Connection  connexionDB(){
+		Connection db = null;
+		String url;
+		String user;
+		String password;
+		
 	    try{
 	        Class.forName("com.mysql.cj.jdbc.Driver");
 	        
-	        String url="jdbc:mysql://localhost/school?serverTimezone="+Calendar.getInstance().getTimeZone().getID();
-	        String user="root";
-	        String password="";
-	       Connection cnx=DriverManager.getConnection(url,user,password);
-	        System.out.println("Connexion bien établié");
-	        return cnx;
+	        url="jdbc:mysql://localhost/school?serverTimezone="+Calendar.getInstance().getTimeZone().getID();
+	        user="root";
+	        password="";
+	        
+	        db = DriverManager.getConnection(url,user,password);
+	        
+	        return db;
 	        
 	    }catch(Exception e){
 	        e.printStackTrace();
